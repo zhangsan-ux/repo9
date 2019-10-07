dc508fd574a47b70e03b3c8ee22b051cbf4f5382
zhangsan <437107708@qq.com>
2019-10-07 19:09:19 +0800

    修改第10行代码

--- git_test/src/com/ieheima/gittest/Test.java
+++ git_test/src/com/ieheima/gittest/Test.java
@@ -7,7 +7,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git project!!!");
         System.out.println("hello git project!!!");
-        System.out.println("hello git project!!!");
+        System.out.println("hel git project!!!");
         System.out.println("hello git project!!!");
         System.out.println("hello git project!!!");
         System.out.println("hello git project!!!");
