85d885f8e4037b01032c33a86aa546cd4fa595b7
zhangsan <437107708@qq.com>
2019-10-07 18:44:20 +0800

    向本地创库提交gittest工程

---
+++ git_test/src/com/ieheima/gittest/Test.java
@@ -0,0 +1,19 @@
+package com.ieheima.gittest;
+
+/**
+ * Create By cgc6868 on 2019/10/7
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+        System.out.println("hello git project!!!");
+
+
+    }
+
+
+}
